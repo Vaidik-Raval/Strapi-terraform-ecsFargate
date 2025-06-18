adbfe881cf6874d2408225c482f0b26bdb71fdaf
Vaidik-Raval <Vaidik-Raval>
2025-06-18 15:32:43 +0530

    Update the code

--- terraform/security.tf
+++ terraform/security.tf
@@ -3,7 +3,6 @@ resource "aws_security_group" "strapi_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    description = "Allow inbound HTTP traffic to Strapi"
     from_port   = var.app_port
     to_port     = var.app_port
     protocol    = "tcp"
